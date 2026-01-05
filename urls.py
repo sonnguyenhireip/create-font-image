80140c3a64741dfe753b352a439b66ed3b685007
TechVerse Developer <hongsonit10@gmail.com>
2026-01-05 13:49:30 +0700

    Update font URLs for thumbnail generation to include BowlbyOne and BowlbyOneSC fonts

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 # List of font URLs to generate thumbnails for
 font_urls = [
-   "https://fonts.hiresdot.com/Adobeblank/AdobeBlank-Regular.woff2"
+    "https://fonts.hiresdot.com/Bowlbyone/BowlbyOne-Regular.woff2",
+    "https://fonts.hiresdot.com/Bowlbyonesc/BowlbyOneSC-Regular.woff2",
 ]
